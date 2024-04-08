99188c94c0cc809acc6963c076061d34a14d426d
Candace Camarillo <candace.fox.camarillo@gmail.com>
2024-04-08 00:22:15 +0000

    all the latest

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,19 +1,18 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:5173'],
-      methods: ['POST'],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept']
+      origin: ['*'],
     }
   },
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
-  'strapi::public',
+  'strapi::public'
 ];
+ 
